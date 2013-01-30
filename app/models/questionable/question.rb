4067668b928841d2cd2eda4ebae3c5b149987529
Nick Urban <nickurban@gmail.com>
2013-01-30 17:52:55 -0500

    sort options by position

--- app/models/questionable/question.rb
+++ app/models/questionable/question.rb
@@ -1,6 +1,6 @@
 module Questionable
   class Question < ActiveRecord::Base
-    has_many :options
+    has_many :options, :order => 'questionable_options.position ASC'
     has_many :assignments
     has_many :subjects, :through => :assignments
     has_many :answers, :through => :assignments
