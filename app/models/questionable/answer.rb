d835b9d6166d45f08e5e73b738ba295f664863f1
Nick Urban <nickurban@gmail.com>
2013-02-05 11:25:41 -0500

    add date_answer method to handle invalid dates

--- app/models/questionable/answer.rb
+++ app/models/questionable/answer.rb
@@ -7,20 +7,12 @@ module Questionable
     belongs_to :option
     has_one :question, :through => :assignment
 
-=begin
-    def self.build_answers_for_subject(user, subject)
-      if subject.kind_of?(Symbol) or subject.kind_of?(String)
-        assignments = Questionable::Assignment.where(:subject_type => subject)
-      else
-        assignments = Questionable::Assignment.where(:subject_type => subject.class.to_s, :subject_id => subject.id)
-      end
+    def date_answer
+      return nil if message.nil?
 
-      assignments = assignments.order(:position)
-      assignments.map { |as| as.answers_for_user(user) }
+      self.message.to_date
+    rescue ArgumentError
+      nil
     end
-=end
-
-    #  Questionable::Question.joins('INNER JOIN questionable_assignments ON questionable_assignments.question_id = questionable_questions.id').where(:questionable_assignments => { :subject_type => type }).order('questionable_assignments.position')
-
   end
 end
---
+++ spec/models/questionable/answer_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+module Questionable
+  describe Answer do
+    let(:answer) { create(:answer) }
+
+    describe "#date_answer" do
+      subject { answer.date_answer }
+
+      context "when the message is a valid date" do
+        let(:answer) { create(:answer, message: '2012-10-11') }
+        it { should be_a Date }
+      end
+
+      context "when the message is an invalid date" do
+        let(:answer) { create(:answer, message: '2012-40-11') }
+        it { should be_nil }
+      end
+
+      context "when the message is null" do
+        let(:answer) { create(:answer, message: nil) }
+        it { should be_nil }
+      end
+    end
+  end
+end
