06e948ba0f908b93ab21017cf221557ca34d4392
Jared Hales <jared@bespokepost.com>
2014-08-21 18:21:34 -0400

    some convenience methods for questionable models

--- app/models/questionable/answer.rb
+++ app/models/questionable/answer.rb
@@ -12,5 +12,19 @@ module Questionable
     rescue ArgumentError
       nil
     end
+
+    def answer_summary
+      if self.option && self.message
+        "#{self.option.title} (#{self.message})"
+      elsif self.message && self.question.date_question?
+        self.date_answer
+      elsif self.option
+        self.option.title
+      else
+        self.message
+      end
+    end
+
+
   end
 end
