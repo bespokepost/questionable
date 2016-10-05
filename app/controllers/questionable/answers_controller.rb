d7b8b7e6d4c966cfc20c226b5d9815a4c1b9a57c
ascoppa <alfredo.scoppa@gmail.com>
2016-10-05 10:03:45 -0300

    Makes answers controller respond to JSON.
    Also: 
    
    - Cleanups create action. (Updates spec)
    - Extracts date answer validations into an active model.
    - Adds a bunch of helpers to question (to check the question type).

--- app/controllers/questionable/answers_controller.rb
+++ app/controllers/questionable/answers_controller.rb
@@ -5,40 +5,34 @@ module Questionable
     def create
       answers = params[:answers]
 
-      unless current_user and answers.is_a?(Hash)
-        flash[:warn] = 'Something went wrong, please try again'
-        return redirect_to_back
-      end
+      return render_error unless current_user && answers.is_a?(Hash)
 
       # Answers should always be a hash, and the values should be arrays,
       # even if the question input_type only supports a single answer.
-      # TODO: Refactor that in a model with validations
-
       answers.each do |assignment_id, answers|
         assignment = Assignment.find(assignment_id)
         assignment.answers.where(user_id: current_user.id).delete_all
+        question = assignment.question
 
-        input_type = assignment.question.input_type
-
-        if input_type == 'string'
+        if question.string?
           message = answers.first || ''
           assignment.answers.create(user_id: current_user.id, message: message[0..255])
-        elsif input_type == 'date'
-          date = answers.first
-          if date[:year].present? or date[:month].present? or date[:day].present?
-            if date[:year].present? and date[:month].present? and date[:day].present?
-              answer = assignment.answers.build(user_id: current_user.id, message: "#{date[:year]}-#{date[:month]}-#{date[:day]}")
-              if answer.date_answer.nil?
-                flash[:warn] = 'Could not save date. Invalid date.'
-              else
-                answer.save
-              end
-            else
-              flash[:warn] = 'Could not save date. You did not select all three fields.'
+        elsif question.date?
+          date_answer = Questionable::DateAnswer.new(answers.first)
+          next if date_answer.empty?
+
+          if date_answer.valid?
+            answer = assignment.answers.build(user_id: current_user.id, message: date_answer.to_s)
+
+            unless answer.save
+              flash[:warn] = 'Could not save date. Invalid date.'
             end
+          else
+            flash[:warn] = 'Could not save date. You did not select all three fields or you entered an invalid date.'
           end
         else
           option_ids = answers
+
           option_ids.each do |oid|
             unless oid.blank?
               assignment.answers.create(user_id: current_user.id, option_id: oid)
@@ -47,10 +41,22 @@ module Questionable
         end
       end
 
-      redirect_to_back
+      respond_to do |format|
+        format.html { redirect_to_back }
+        format.json { render json: { message: flash[:warn] }, status: :ok }
+      end
     end
 
     private
+
+    def render_error
+      flash[:warn] = 'Something went wrong, please try again'
+
+      respond_to do |format|
+        format.html { redirect_to_back }
+        format.json { render json: { error: flash[:warn] }, status: :unprocessable_entity }
+      end
+    end
 
     def redirect_to_back
       if request.env["HTTP_REFERER"].present?
